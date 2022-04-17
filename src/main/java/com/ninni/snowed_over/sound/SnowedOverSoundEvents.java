7c2dfb02e6760d894cf7b6d7dc310ed2d552c3b7
N1nn1 <64173061+N1nn1@users.noreply.github.com>
2022-04-17 15:57:18 +0200

    * interface noises *

--- src/main/java/com/ninni/snowed_over/sound/SnowedOverSoundEvents.java
+++ src/main/java/com/ninni/snowed_over/sound/SnowedOverSoundEvents.java
@@ -5,18 +5,18 @@ import net.minecraft.sound.SoundEvent;
 import net.minecraft.util.Identifier;
 import net.minecraft.util.registry.Registry;
 
-public class SnowedOverSoundEvents {
+public interface SnowedOverSoundEvents {
 
-    public static final SoundEvent ITEM_PINECONE_EAT = pinecone("eat");
+    SoundEvent ITEM_PINECONE_EAT = pinecone("eat");
     private static SoundEvent pinecone(String type) {
         return createItemSound("pinecone", type);
     }
 
-    public static final SoundEvent BLOCK_COMPACTED_SNOW_BREAK = compacted_snow("break");
-    public static final SoundEvent BLOCK_COMPACTED_SNOW_STEP = compacted_snow("step");
-    public static final SoundEvent BLOCK_COMPACTED_SNOW_PLACE = compacted_snow("place");
-    public static final SoundEvent BLOCK_COMPACTED_SNOW_HIT = compacted_snow("hit");
-    public static final SoundEvent BLOCK_COMPACTED_SNOW_FALL = compacted_snow("fall");
+    SoundEvent BLOCK_COMPACTED_SNOW_BREAK = compacted_snow("break");
+    SoundEvent BLOCK_COMPACTED_SNOW_STEP = compacted_snow("step");
+    SoundEvent BLOCK_COMPACTED_SNOW_PLACE = compacted_snow("place");
+    SoundEvent BLOCK_COMPACTED_SNOW_HIT = compacted_snow("hit");
+    SoundEvent BLOCK_COMPACTED_SNOW_FALL = compacted_snow("fall");
     private static SoundEvent compacted_snow(String type) {
         return createBlockSound("compacted_snow", type);
     }
