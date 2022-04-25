07d961cf1fbddc876ce30d4d4336410525961710
N1nn1 <64173061+N1nn1@users.noreply.github.com>
2022-04-25 14:55:36 +0200

    Custom baby penguin scaling

--- src/main/java/com/ninni/snowed_over/client/model/entity/PenguinEntityModel.java
+++ src/main/java/com/ninni/snowed_over/client/model/entity/PenguinEntityModel.java
@@ -9,7 +9,9 @@ import net.minecraft.client.model.ModelPartBuilder;
 import net.minecraft.client.model.ModelPartData;
 import net.minecraft.client.model.ModelTransform;
 import net.minecraft.client.model.TexturedModelData;
+import net.minecraft.client.render.VertexConsumer;
 import net.minecraft.client.render.entity.model.AnimalModel;
+import net.minecraft.client.util.math.MatrixStack;
 import net.minecraft.util.math.MathHelper;
 
 @SuppressWarnings("FieldCanBeLocal, unused")
@@ -27,7 +29,6 @@ public class PenguinEntityModel<T extends PenguinEntity> extends AnimalModel<Pen
 
 
     public PenguinEntityModel(ModelPart root) {
-        super(false, 5.0F, 0.0F);
         this.root = root;
 
         this.body         = root.getChild("body");
@@ -205,12 +206,31 @@ public class PenguinEntityModel<T extends PenguinEntity> extends AnimalModel<Pen
 
 
     @Override
+    public void render(MatrixStack matrices, VertexConsumer vertices, int light, int overlay, float red, float green, float blue, float alpha) {
+        if (this.child) {
+            matrices.push();
+            matrices.scale(0.55F, 0.55F, 0.55F);
+            matrices.translate(0.0, 1.335F, 0);
+            this.head.render(matrices, vertices, light, overlay, red, green, blue, alpha);
+            matrices.pop();
+            matrices.push();
+            matrices.scale(0.45F, 0.45F, 0.45F);
+            matrices.translate(0.0, 1.84, 0.0);
+            ImmutableList.of(body, leftLeg, rightLeg).forEach((part) -> part.render(matrices, vertices, light, overlay, red, green, blue, alpha));
+            matrices.pop();
+        } else {
+            ImmutableList.of(body, leftLeg, rightLeg, head).forEach((part) -> part.render(matrices, vertices, light, overlay, red, green, blue, alpha));
+        }
+
+    }
+
+    @Override
     protected Iterable<ModelPart> getHeadParts() {
-        return ImmutableList.of(this.head);
+        return ImmutableList.of(head);
     }
 
     @Override
     protected Iterable<ModelPart> getBodyParts() {
-        return ImmutableList.of(this.body, this.leftLeg, this.rightLeg);
+        return ImmutableList.of(body, leftLeg, rightLeg);
     }
 }
