6da60e617baf38427f0a088578356f7b5448faeb
0rc1nus <mitchellctlui@gmail.com>
2022-04-28 10:31:10 +0800

    aaa enchantment

--- src/main/java/com/ninni/snowed_over/enchantments/HastyHoovesEnchantment.java
+++ src/main/java/com/ninni/snowed_over/enchantments/HastyHoovesEnchantment.java
@@ -1,8 +1,10 @@
 package com.ninni.snowed_over.enchantments;
 
+import com.ninni.snowed_over.item.SnowedOverItems;
 import net.minecraft.enchantment.Enchantment;
 import net.minecraft.enchantment.EnchantmentTarget;
 import net.minecraft.entity.EquipmentSlot;
+import net.minecraft.item.ItemStack;
 
 public class HastyHoovesEnchantment extends Enchantment {
 
@@ -15,4 +17,18 @@ public class HastyHoovesEnchantment extends Enchantment {
         return 4;
     }
 
+    @Override
+    public int getMinPower(int level) {
+        return 5 + (level - 1) * 8;
+    }
+
+    @Override
+    public int getMaxPower(int level) {
+        return super.getMaxPower(level) + 50;
+    }
+
+    @Override
+    public boolean isAcceptableItem(ItemStack stack) {
+        return stack.getItem() == SnowedOverItems.HOOF_ARMOR && super.isAcceptableItem(stack);
+    }
 }
