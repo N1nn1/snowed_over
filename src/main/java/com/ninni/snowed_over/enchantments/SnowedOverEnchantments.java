3cd62689310e50cc5fb71d88611858aec82d5a12
0rc1nus <mitchellctlui@gmail.com>
2022-04-30 19:52:20 +0800

    Enchantment Registry

--- src/main/java/com/ninni/snowed_over/enchantments/SnowedOverEnchantments.java
+++ src/main/java/com/ninni/snowed_over/enchantments/SnowedOverEnchantments.java
@@ -1,15 +1,16 @@
 package com.ninni.snowed_over.enchantments;
 
 import com.ninni.snowed_over.SnowedOver;
+import net.minecraft.enchantment.Enchantment;
 import net.minecraft.util.Identifier;
 import net.minecraft.util.registry.Registry;
 
 public class SnowedOverEnchantments {
 
-    public static final HastyHoovesEnchantment HASTY_HOOVES = new HastyHoovesEnchantment();
+    public static final Enchantment HASTY_HOOVES = register("hasty_hooves", new HastyHoovesEnchantment());
 
-    public static void init() {
-        Registry.register(Registry.ENCHANTMENT, new Identifier(SnowedOver.MOD_ID, "hasty_hooves"), HASTY_HOOVES);
+    public static Enchantment register(String name, Enchantment enchantment) {
+        return Registry.register(Registry.ENCHANTMENT, new Identifier(SnowedOver.MOD_ID, name), enchantment);
     }
 
 }
