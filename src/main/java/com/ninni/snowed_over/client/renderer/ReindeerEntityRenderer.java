2f5cf7af732110e9ff4d8f3a74b65f16d0901e6d
N1nn1 <64173061+N1nn1@users.noreply.github.com>
2022-05-10 03:56:15 +0200

    Nvm!

--- src/main/java/com/ninni/snowed_over/client/renderer/ReindeerEntityRenderer.java
+++ src/main/java/com/ninni/snowed_over/client/renderer/ReindeerEntityRenderer.java
@@ -11,7 +11,6 @@ import net.minecraft.client.render.entity.EntityRendererFactory;
 import net.minecraft.client.render.entity.MobEntityRenderer;
 import net.minecraft.client.render.entity.feature.SaddleFeatureRenderer;
 import net.minecraft.entity.LivingEntity;
-import net.minecraft.util.Formatting;
 import net.minecraft.util.Identifier;
 
 import java.util.Calendar;
@@ -23,24 +22,21 @@ public class ReindeerEntityRenderer<T extends LivingEntity> extends MobEntityRen
     public static final Identifier TEXTURE = new Identifier(MOD_ID, "textures/entity/reindeer/reindeer.png");
     public static final Identifier TEXTURE_FESTIVE = new Identifier(MOD_ID, "textures/entity/reindeer/reindeer_festive.png");
     private boolean festivity;
-    private static ReindeerEntity reindeer;
 
     public ReindeerEntityRenderer(EntityRendererFactory.Context ctx) {
         super(ctx, new ReindeerEntityModel<>(ctx.getPart(SnowedOverEntityModelLayers.REINDEER)), 0.6F);
+        Calendar calendar = Calendar.getInstance();
         this.addFeature(new ReindeerArmorFeatureRenderer<>(this, new ReindeerEntityModel<>(ctx.getPart(SnowedOverEntityModelLayers.REINDEER_ARMOR)), new Identifier(MOD_ID, "textures/entity/reindeer/reindeer_armor.png")));
         this.addFeature(new SaddleFeatureRenderer<>(this, new ReindeerEntityModel<>(ctx.getPart(SnowedOverEntityModelLayers.REINDEER_ARMOR)), new Identifier(MOD_ID, "textures/entity/reindeer/reindeer_saddle.png")));
-
-        Calendar calendar = Calendar.getInstance();
-        if (calendar.get(Calendar.MONTH) + 1 == 12 && calendar.get(Calendar.DATE) >= 24 && calendar.get(Calendar.DATE) <= 26 || calendar.get(Calendar.MONTH) + 1 == 8 && calendar.get(Calendar.DATE) == 2 || "Rudolph".equals(Formatting.strip(reindeer.getName().getString()))) {
+        if (calendar.get(Calendar.MONTH) + 1 == 12 && calendar.get(Calendar.DATE) >= 24 && calendar.get(Calendar.DATE) <= 26 || calendar.get(Calendar.MONTH) + 1 == 8 && calendar.get(Calendar.DATE) == 2) {
             this.addFeature(new ReindeerFestiveOverlayFeatureRenderer(this));
             this.festivity = true;
-        } //checks if it's Christmas, if it's the world reindeer day or if the reindeer's caller Rudolph to apply a custom skin
+        } //checks if it's Christmas or if it's the world reindeer day to apply a custom skin
     }
 
     @Override
     public Identifier getTexture(ReindeerEntity entity) {
-        String string = Formatting.strip(entity.getName().getString());
-        if (festivity || "Rudolph".equals(Formatting.strip(entity.getName().getString()))) { return TEXTURE_FESTIVE;}
+        if (festivity) { return TEXTURE_FESTIVE;}
         return TEXTURE;
     }
 }
