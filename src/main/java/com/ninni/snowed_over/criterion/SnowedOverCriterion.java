c4dd80113b3cd744efa4bd9e96e8b645ff947345
0rc1nus <mitchellctlui@gmail.com>
2022-05-21 16:00:22 +0800

    Refactoring

--- src/main/java/com/ninni/snowed_over/criterion/SnowedOverCriterion.java
+++ src/main/java/com/ninni/snowed_over/criterion/SnowedOverCriterion.java
@@ -9,16 +9,16 @@ import net.minecraft.advancements.critereon.SimpleCriterionTrigger;
 import net.minecraft.resources.ResourceLocation;
 import net.minecraft.server.level.ServerPlayer;
 
-public class SOCriterion extends SimpleCriterionTrigger<SOCriterion.Conditions> {
+public class SnowedOverCriterion extends SimpleCriterionTrigger<SnowedOverCriterion.TriggerInstance> {
     private final ResourceLocation ID;
 
-    public SOCriterion(String name) {
+    public SnowedOverCriterion(String name) {
         ID = new ResourceLocation(SnowedOver.MOD_ID, name);
     }
 
     @Override
-    protected Conditions createInstance(JsonObject jsonObject, EntityPredicate.Composite composite, DeserializationContext deserializationContext) {
-        return new SOCriterion.Conditions(ID, composite);
+    protected TriggerInstance createInstance(JsonObject jsonObject, EntityPredicate.Composite composite, DeserializationContext deserializationContext) {
+        return new SnowedOverCriterion.TriggerInstance(ID, composite);
     }
 
     @Override
@@ -27,11 +27,11 @@ public class SOCriterion extends SimpleCriterionTrigger<SOCriterion.Conditions> 
     }
 
     public void trigger(ServerPlayer player) {
-        this.trigger(player, (conditions)->true);
+        this.trigger(player, conditions -> true);
     }
 
-    public static class Conditions extends AbstractCriterionTriggerInstance {
-        public Conditions(ResourceLocation resourceLocation, EntityPredicate.Composite composite) {
+    public static class TriggerInstance extends AbstractCriterionTriggerInstance {
+        public TriggerInstance(ResourceLocation resourceLocation, EntityPredicate.Composite composite) {
             super(resourceLocation, composite);
         }
     }
--- src/main/java/com/ninni/snowed_over/init/SnowedOverCriteriaTriggers.java
+++ src/main/java/com/ninni/snowed_over/init/SnowedOverCriteriaTriggers.java
@@ -1,13 +1,13 @@
 package com.ninni.snowed_over.init;
 
 import com.ninni.snowed_over.SnowedOver;
-import com.ninni.snowed_over.criterion.SOCriterion;
+import com.ninni.snowed_over.criterion.SnowedOverCriterion;
 import net.minecraft.advancements.CriteriaTriggers;
 import net.minecraftforge.fml.common.Mod;
 
 @Mod.EventBusSubscriber(modid = SnowedOver.MOD_ID)
 public class SnowedOverCriteriaTriggers {
 
-    public static final SOCriterion CLOUD_JUMPER_BOOST = CriteriaTriggers.register(new SOCriterion("cloud_jumper_boost"));
+    public static final SnowedOverCriterion CLOUD_JUMPER_BOOST = CriteriaTriggers.register(new SnowedOverCriterion("cloud_jumper_boost"));
 
 }
